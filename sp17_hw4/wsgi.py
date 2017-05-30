c4641e19eff86cb4e8e76b85f49f0dc46ece6abb
wilson8507 <wilson8507@gmail.com>
2017-05-30 22:36:14 +0800

    init commit

---
+++ sp17_hw4/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for sp17_hw4 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sp17_hw4.settings")
+
+application = get_wsgi_application()
